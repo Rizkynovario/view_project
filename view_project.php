dbe590e9774933f60c655b3ed9cbf2a54aae4433
Rizkynovario <74966471+Rizkynovario@users.noreply.github.com>
2020-12-15 03:40:42 -0800

    Add files via upload

---
+++ view_project.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html> 
+<html> 
+<head>     
+	<meta charset="utf-8">     
+	<title>Web Prog II | Toko Sepatu Rasafinderis</title>     
+	<link rel="stylesheet" type="text/css" href="http://localhost/pustaka-booking/assets/css/stylebaju.css">
+</head> 
+<body>     
+	<div class="container">
+	<div class="view">
+				
+		<div class="navbar">         
+		<header>             
+			<hgroup>                 
+				<h1>Website Toko Bju</h1>                 
+				<h3>SELAMAT DATANG DI TOKO RASAFINDERIS</h3>             
+			</hgroup>             
+			<nav>                 
+				<ul>                     
+					<li><a href="<?php echo base_url().'index.php/tokobaju' ?>">Beranda</a></li>                     
+					<li><a href="<?php echo base_url().'index.php/tokobaju/galery' ?>">Galery</a></li>
+					<li><a href="<?php echo base_url().'index.php/tokobaju/profil' ?>">Profil</a></li>
+					<li><a href="<?php echo base_url().'index.php/tokobaju/kontak' ?>">Kontak</a></li>                 
+				</ul>             
+			</nav>             
+			<div class="clear"></div>         
+		</header>  
+		<section>
+	<form class="searchform">
+	<input type="text" placeholder="Search">
+	<button>Search</button>
+</form>
+
+		</div>
+		<div class="runningtext">
+			<marquee behavior="scroll" scrollamount="3" onmouseover="this.stop();" onmouseout="this.start();" direction="left">
+				SELAMAT DATANG DI TOKO BAJU RASAFINDERIS
+			</marquee>
+		</div>
+	</div>
+	<div class="gambar">
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju1.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju2.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju3.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju4.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju5.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju6.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju7.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju8.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju9.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju10.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju11.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju12.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju13.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju14.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju15.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju16.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju17.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju18.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju19.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju20.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju21.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju22.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju23.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju24.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju25.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju26.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju27.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju28.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju29.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+		</div>
+
+		<div class="foto">
+			<img src="http://localhost/pustaka-booking/assets/img/gambar/baju30.jpg">
+			<p>Harga 100.000</p>
+			<a href="">Beli Sekarang</a>
+	</div>
+</section>
+<footer>         
+             <a href="http://www.tokobaju.com">Toko Baju Rasafinderis</a>        
+         </footer>     
+     </div>
+   
+</body> 
+</html> 
